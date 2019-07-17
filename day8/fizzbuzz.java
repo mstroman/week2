12803a3588749328c59300c021ab5fef72809d84
mstroman <52668145+mstroman@users.noreply.github.com>
2019-07-17 13:59:34 -0400

    Add files via upload

---
+++ day8/fizzbuzz.java
@@ -0,0 +1,26 @@
+package day8;
+
+public class fizzbuzz {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	for (int x=1; x<=100; x++) {
+		System.out.println(x);
+
+		 if (x%3 == 0 && x%5 ==0 ) {
+		System.out.println("fizz buzz");
+		
+	}else if (x%5 == 0) {
+		System.out.println("Buzz");
+	
+	} else if(x%3 ==0 ) {
+	System.out.println("fizz");{
+	}
+	
+	}
+}
+
+}
+}
